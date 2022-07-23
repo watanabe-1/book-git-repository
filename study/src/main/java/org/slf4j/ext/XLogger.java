304428d75180845334511ae9c958ba1878c566ee
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2022-07-23 20:14:31 +0900

    コメントの追加

--- study/src/main/java/org/slf4j/ext/XLogger.java
+++ study/src/main/java/org/slf4j/ext/XLogger.java
@@ -16,72 +16,159 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
  */
 public class XLogger {
 
+  /**
+   * ログID未定義時のログメッセージ
+   */
   private static final String UNDEFINED_MESSAGE_FORMAT = "UNDEFINED-MESSAGE id:{0} arg:{1}";
+
   private static final String LOG_MESSAGE_FORMAT = "[{0}] {1}";
-  // クラスパスからの参照に書き方を統一のためReloadableResourceBundleMessageSourceに変更
-  // private static ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+
+  /**
+   * MessageSourceでログメッセージを取得する
+   */
   private static ReloadableResourceBundleMessageSource messageSource =
       new ReloadableResourceBundleMessageSource();
 
+  // staticイニシャライザにてMessageSourceを生成する
   static {
+    // プロパティファイルをパースする際に使用する文字コードを指定
     messageSource.setDefaultEncoding("UTF-8");
+    // i18nに配置したLogMessages.propertiesを読み込む
     messageSource.setBasenames("classpath:config/properties/i18n/LogMessages");
   }
 
+  /**
+   * ロガー
+   */
   private final Logger logger;
 
+  /**
+   * コンストラクター<br/>
+   * SLF4Jを使用
+   * 
+   * @param clazz クラス
+   */
   public XLogger(Class<?> clazz) {
     logger = LoggerFactory.getLogger(clazz);
   }
 
+  /**
+   * DEBUGレベルのログ出力を許可しているか、判定する。
+   * 
+   * @return 判定結果
+   */
   public boolean isDebugEnabled() {
     return logger.isDebugEnabled();
   }
 
+  /**
+   * DEBUGレベルのログはそのままメッセージを出力
+   * 
+   * @param format フォーマット
+   * @param args 埋め込み文字
+   */
   public void debug(String format, Object... args) {
     logger.debug(format, args);
   }
 
+  /**
+   * INFOレベルのログ<br/>
+   * 外部ファイルからのメッセージを読み込む
+   * 
+   * @param id メッセージID
+   * @param args 埋め込み文字
+   */
   public void info(String id, Object... args) {
     if (logger.isInfoEnabled()) {
       logger.info(createLogMessage(id, args));
     }
   }
 
+  /**
+   * WARNレベルのログ<br/>
+   * 外部ファイルからのメッセージを読み込む
+   * 
+   * @param id メッセージID
+   * @param args 埋め込み文字
+   */
   public void warn(String id, Object... args) {
     if (logger.isWarnEnabled()) {
       logger.warn(createLogMessage(id, args));
     }
   }
 
+  /**
+   * ERRORレベルのログ<br/>
+   * 外部ファイルからのメッセージを読み込む
+   * 
+   * @param id メッセージID
+   * @param args 埋め込み文字
+   */
   public void error(String id, Object... args) {
     if (logger.isErrorEnabled()) {
       logger.error(createLogMessage(id, args));
     }
   }
 
+  /**
+   * TRACEレベルのログ<br/>
+   * 外部ファイルからのメッセージを読み込む
+   * 
+   * @param id メッセージID
+   * @param args 埋め込み文字
+   */
   public void trace(String id, Object... args) {
     if (logger.isTraceEnabled()) {
       logger.trace(createLogMessage(id, args));
     }
   }
 
+  /**
+   * TRACEレベルのログ<br/>
+   * 外部ファイルからのメッセージを読み込む
+   * 
+   * @param id メッセージID
+   * @param t the exception (throwable) to log
+   * @param args 埋め込み文字
+   */
   public void warn(String id, Throwable t, Object... args) {
     if (logger.isWarnEnabled()) {
       logger.warn(createLogMessage(id, args), t);
     }
   }
 
+  /**
+   * ERRORレベルのログ<br/>
+   * 外部ファイルからのメッセージを読み込む
+   * 
+   * @param id メッセージID
+   * @param t the exception (throwable) to log
+   * @param args 埋め込み文字
+   */
   public void error(String id, Throwable t, Object... args) {
     if (logger.isErrorEnabled()) {
       logger.error(createLogMessage(id, args), t);
     }
   }
 
+  /**
+   * ログメッセージの作成
+   * 
+   * @param id メッセージID
+   * @param args 埋め込み文字
+   * @return ログメッセージ
+   */
   private String createLogMessage(String id, Object... args) {
     return MessageFormat.format(LOG_MESSAGE_FORMAT, id, getMessage(id, args));
   }
 
+  /**
+   * ログメッセージの取得
+   * 
+   * @param id メッセージID
+   * @param args 埋め込み文字
+   * @return ログメッセージ
+   */
   private String getMessage(String id, Object... args) {
     String message;
     try {
@@ -89,6 +176,7 @@ public class XLogger {
     } catch (NoSuchMessageException e) {
       message = MessageFormat.format(UNDEFINED_MESSAGE_FORMAT, id, Arrays.toString(args));
     }
+
     return message;
   }
 }
