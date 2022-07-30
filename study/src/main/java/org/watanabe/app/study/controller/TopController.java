34447e66593808b550ec26bd178e3ed6a4305be9
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2022-07-31 00:59:54 +0900

    gradleプロジェクトに変更

--- study/src/main/java/org/watanabe/app/study/controller/TopController.java
+++ study/src/main/java/org/watanabe/app/study/controller/TopController.java
@@ -20,7 +20,7 @@ public class TopController {
     // model.addObject("message", "Hello World!!");
 
     // log.debug("debug log");
-    log.info("i.ab.cd.1001", "study -- トップ --");
+    log.info("i.ab.cd.1001", "study -- トップです！！！ --");
     // log.warn("w.ab.cd.2001", "replace_value_2");
     // log.error("e.ab.cd.3001", "replace_value_3");
     // log.trace("t.ab.cd.4001", "replace_value_4");
