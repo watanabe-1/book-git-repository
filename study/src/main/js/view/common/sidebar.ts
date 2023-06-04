63007e32bd7d343d23c5d6578a38ff6632c9fea7
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2023-06-04 23:35:34 +0900

    sidebarのメニューボタンクリックしたときに動く動作をbootstrapを使用したものに変更

--- study/src/main/js/view/common/sidebar.ts
+++ study/src/main/js/view/common/sidebar.ts
@@ -1,5 +1,5 @@
 import '../../common/common';
-import * as studyUtil from './../../study/util/studyUtil';
+import { swichClass } from '../../study/util/studyUtil';
 
 // CSS script ボタンクリック時にクラスがなかったら付与あったら削除することで、出し入れを行う
 const menu_btn: HTMLAnchorElement = document.querySelector(
@@ -7,13 +7,9 @@ const menu_btn: HTMLAnchorElement = document.querySelector(
 ) as HTMLAnchorElement;
 if (menu_btn) {
   const menu_btn_i: HTMLElement = menu_btn.querySelector('i');
-  const sidebar: HTMLElement = document.querySelector('#sidebarMenu');
-  const container: HTMLElement = document.querySelector('.my-container');
   menu_btn.addEventListener('click', () => {
-    if (sidebar && container) {
-      sidebar.classList.toggle('active-nav');
-      container.classList.toggle('active-cont');
-      studyUtil.swichClass(menu_btn_i, 'bi-text-left', 'bi-text-right');
+    if (menu_btn_i) {
+      swichClass(menu_btn_i, 'bi-text-left', 'bi-text-right');
     }
   });
 }
