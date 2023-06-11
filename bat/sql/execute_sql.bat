6edb6b96e389fe20365632274c94cf490767a65d
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2023-06-11 23:07:24 +0900

    コメントを修正

--- bat/sql/execute_sql.bat
+++ bat/sql/execute_sql.bat
@@ -3,7 +3,10 @@ rem @echo off
 rem 文字コードをUTF-8に変更
 chcp 65001
 
-REM 現在時刻を設定します
+rem カレントディレクトリを、現在実行しているファイルのあるフォルダへと移動させる
+cd /d %~dp0
+
+REM 現在時刻を設定
 set year=%date:~0,4%
 set month=%date:~5,2%
 set day=%date:~8,2%
@@ -11,21 +14,18 @@ set hour=%time:~0,2%
 set minute=%time:~3,2%
 set second=%time:~6,2%
 
-REM ログファイルの出力パスを設定します
+REM ログファイルの出力パスを設定
 set LOG_FILE=./logs/%year%-%month%-%day%_%hour%-%minute%-%second%.log
-
-REM ログファイルを作成します    
-rem echo test %LOG_FILE%
 
 echo ------------------------ >> %LOG_FILE%
 echo execute_sql.bat start >> %LOG_FILE%
 echo %date% %time% >> %LOG_FILE%
 echo ------------------------ >> %LOG_FILE%
 
-REM db接続情報を読み込むためのファイルパスを設定します
+REM db接続情報を読み込むためのファイルパスを設定
 set DB_CONFIG_FILE=./config/db_config.txt
 
-REM ファイルの存在を確認します
+REM ファイルの存在を確認
 if not exist "%DB_CONFIG_FILE%" (
     echo Configuration file %DB_CONFIG_FILE% not found. Exiting... >> %LOG_FILE%
     exit /b
@@ -36,17 +36,17 @@ for /f "usebackq delims=" %%x in ("%DB_CONFIG_FILE%") do (
     set "%%x"
 )
 
-REM データベースに接続できるか確認します
+REM データベースに接続できるか確認
 psql -U %PGUSER% -d %PGDATABASE% -p %PGPORT% -h %PGHOST% -c "SELECT 1;" 
 if %errorlevel% neq 0 (
     echo Failed to connect to the database. >> %LOG_FILE%
     exit /b
 )
 
-REM SQLファイルリストを読み込むためのファイルパスを設定します
+REM SQLファイルリストを読み込むためのファイルパスを設定
 set SQL_LIST_FILE=./config/sql_list.txt
 
-REM ファイルの存在を確認します
+REM ファイルの存在を確認
 if not exist "%SQL_LIST_FILE%" (
     echo SQL file list %SQL_LIST_FILE% not found. Exiting...>> %LOG_FILE%
     exit /b
